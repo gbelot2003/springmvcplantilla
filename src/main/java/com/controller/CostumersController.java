58e91eae93290402ce802399244f43cad5435541
Gerardo Belot <gbelot2003@hotmail.com>
2019-06-08 23:39:13 -0600

    Creando trimmer para la validación de campos

--- src/main/java/com/controller/CostumersController.java
+++ src/main/java/com/controller/CostumersController.java
@@ -2,32 +2,40 @@ package com.controller;
 
 import javax.validation.Valid;
 
+import org.springframework.beans.propertyeditors.StringTrimmerEditor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.WebDataBinder;
+import org.springframework.web.bind.annotation.InitBinder;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
-
 @Controller
 @RequestMapping("/costumers")
 public class CostumersController {
-	
+
+	@InitBinder
+	public void initBinder(WebDataBinder dataBinder) {
+		StringTrimmerEditor stringTrimmerEditor = new StringTrimmerEditor(true);
+		dataBinder.registerCustomEditor(String.class, stringTrimmerEditor);
+	}
+
 	@RequestMapping("/")
-	public ModelAndView show(Model model){
+	public ModelAndView show(Model model) {
 		ModelAndView mv = new ModelAndView("costumers/index");
 		model.addAttribute("customer", new Costumer());
 		return mv;
 	}
-	
+
 	@RequestMapping("/store")
 	public String store(@ModelAttribute("customer") @Valid Costumer costumer, BindingResult results) {
 		System.out.println(results.getErrorCount());
-		if(results.hasErrors()) {
+		if (results.hasErrors()) {
 			return "costumers/index";
 		}
-		return "costumers/confirmation";	
-		
+		return "costumers/confirmation";
+
 	}
 }
